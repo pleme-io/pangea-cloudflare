30449a75804e18bb258e6b99592df6609bb99576
drzzln <drzzln@protonmail.com>
2026-02-22 14:13:54 -0500

    fix: relax Ruby constraint and fix static route test
    - Lower required_ruby_version from 3.4.0 to 3.3.0 (matches Nix env)
    - Fix cloudflare_magic_wan_static_route test with required attributes
    - Regenerate Gemfile.lock and gemset.nix

--- spec/resources/cloudflare_magic_wan_static_route/synthesis_spec.rb
+++ spec/resources/cloudflare_magic_wan_static_route/synthesis_spec.rb
@@ -8,7 +8,12 @@ RSpec.describe 'cloudflare_magic_wan_static_route synthesis' do
   it 'synthesizes' do
     TerraformSynthesizer.new.instance_eval do
       extend Pangea::Resources::Cloudflare
-      cloudflare_magic_wan_static_route(:test, { account_id: "f037e56e89293a057740de681ac9abbe" })
+      cloudflare_magic_wan_static_route(:test, {
+        account_id: "f037e56e89293a057740de681ac9abbe",
+        prefix: "192.0.2.0/24",
+        nexthop: "10.0.0.1",
+        priority: 100
+      })
     end
   end
 end
